1b602889589ce81e2357cd3ad4004f16c80dbf60
Kadri Kaljo <kadrikaljo@gmail.com>
2025-10-24 12:54:37 +0300

    gitignore jne

--- book.php
+++ book.php
@@ -1,17 +1,6 @@
 <?php
 
-$host = '127.0.0.1';
-$db   = 'bookstore';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+require_once('./connection.php');
 
 if (!isset($_GET['id']) || !$_GET['id'] )
  {
@@ -29,4 +18,4 @@ $book = $stmt->fetch();
 
 var_dump($book);
 
-//raamatu info/kirjec   ":ID" placeholder
+//raamatu info/kirjed   ":ID" placeholder
